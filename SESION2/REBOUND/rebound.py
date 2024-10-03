928ef8b90df511cf36ec3da3b14ffa5817e94993
Yuri Urzua Lebuy <yuri19762008@hotmail.com>
2024-10-03 12:26:03 -0300

    cracion clase animal

---
+++ SESION2/REBOUND/rebound.py
@@ -0,0 +1,21 @@
+# --> YURI URZUA LEBUY <--
+#creacion de clase animal
+class Animal:
+    def __init__(self, nombre, raza, edad, peso):
+        self.nombre = nombre
+        self.raza = raza
+        self.edad = edad
+        self.peso = peso
+    
+    def __str__(self):
+        return f"Nombre: {self.nombre}, Raza: {self.raza}, Edad: {self.edad} años, Peso: {self.peso} kg"
+
+# Creando instancias de la clase Animal
+caballo = Animal("Zeus", "Pura sangre", 5, 450)
+leon = Animal("Boulder", "Atlas", 10, 130)
+
+# Imprimiendo la información de los animales
+print("Información del caballo:")
+print(caballo)
+print("\nInformación del león:")
+print(leon)
