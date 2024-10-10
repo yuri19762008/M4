5719a5ee55796a5ea61c6b4681cd0c0eed63e814
Yuri Urzua Lebuy <yuri19762008@hotmail.com>
2024-10-10 11:21:49 -0300

    actualizacion de desarrollo en claes

--- SESION4/herencia/herencia.py
+++ SESION4/herencia/herencia.py
@@ -1,35 +1,52 @@
 class Vehiculo:
-    def __init__(self, marca,modelo, color, peso,puestos):
+    def __init__(self, marca,modelo,color,peso,puestos):
         self.marca = marca
         self.modelo = modelo
         self.color = color
         self.peso = peso
         self.puestos = puestos
-        
+    
     def arrancar(self):
-        print  ("el vehiculo arranco.")
+        print("El vehiculo arrancó.")
     
     def frenar(self):
-        print  ("el vehiculo freno.")
-    
+        print("El vehiculo frenó")
+
     def girar(self):
-        print  ("el vehiculo giro.")
+        print("El vehiculo giró")
     
     def acelerar(self):
-        print  ("el vehiculo acelero.")
+        print("El vehiculo aceleró")
     
     def estacionar(self):
-        print  ("el vehiculo estaciono .")
+        print("El vehiculo estacionó")
 
 class Carro(Vehiculo):
-    
-    def __init__(self, marca, modelo, color, peso, puestos, velocidad, cilindrada):
-        super().__init__(marca, modelo, color, peso, puestos)
+
+    def __init__(self,marca,modelo,color,peso,puestos,velocidad,cilindrada):
+        super().__init__(marca,modelo,color,peso,puestos)
         self.velocidad = velocidad
         self.cilindrada = cilindrada
-        
-        
-carro = Carro ("nissan", "Versa","rojo",2,3,250,2)
+
+    def arrancar(self):
+        print(f"El carro arrancó ")
+
+    def arrancar(self,forma):
+        print(f"el carro arrancó con {forma}")
+
+def arrancar_vehiculo(vehiculos):
+    for vehiculo in vehiculos:
+        vehiculo.arrancar()
 
 
-        
+carro = Carro("nissan","versa","rojo",2,5,250,2)
+vehiculo = Vehiculo("nissan","versa","rojo",2,5)
+
+print(carro.modelo)
+#carro.arrancar()
+
+#print(isinstance(vehiculo,Carro))
+
+arrancar_vehiculo([carro,vehiculo])
+
+carro.arrancar()
