a1d70352c7c182742756100679d77fa735664ec8
Yuri Urzua Lebuy <yuri19762008@hotmail.com>
2024-10-12 18:51:03 -0300

    rebound y drilling 6

---
+++ SESION6/drilling/drilling.py
@@ -0,0 +1,15 @@
+# Definición del diccionario de usuarios
+usuarios = {'001': 'Mark', '002': 'Monica', '003': 'Jacob'}
+
+# ID de usuario que queremos buscar
+id_usuario = '004'
+
+# Intentamos acceder al valor de la clave
+try:
+    print(usuarios[id_usuario])
+except KeyError:
+    print(f'La clave {id_usuario} no está en el diccionario. Añadiendo clave...')
+    usuarios[id_usuario] = 'Ninguno'
+
+# Imprimimos el diccionario actualizado
+print(usuarios)
---
+++ SESION6/rebound/rebound.py
@@ -0,0 +1,10 @@
+# Definición de variables
+suma = 3000
+contador = 0
+
+# Intento de división con manejo de excepción
+try:
+    resultado = suma / contador
+    print(resultado)
+except ZeroDivisionError:
+    print('División por cero.')
